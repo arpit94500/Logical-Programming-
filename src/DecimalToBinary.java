e1412dbea6e5a13326248d841ae4cc6c1ca1968c
Arpit Sharma <arpitsharma79854@gmail.com>
2020-10-04 15:11:09 +0530

    .

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Decimal Number");
+		double n = sc.nextDouble();
+		int a = (int)n;
+		double b = n-a;
+		String res="";
+		if(a==0)
+			res=res+a;
+		while(a>0)
+		{
+			int t = a%2;
+			res = t+ res;
+			a=a/2;		
+		}
+		
+		if(b!=0)
+		{
+			int t=0;
+			res=res+".";
+			while(t<=3)
+			{
+				
+				b= b*2 -  (int)b*2;
+				res= res+ (int)b;
+				t++;
+			}
+		}
+		System.out.println(res);
+	}
+	
+
+}
--- src/Pattern4.java
+++ src/Pattern4.java
@@ -1,37 +1,69 @@
-// Decimal to Binary
-import java.util.*;
+
+/*
+pattern if noOfRows entered is 10:
+1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1
+  1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
+    1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+      1 2 3 4 5 6 7 6 5 4 3 2 1
+        1 2 3 4 5 6 5 4 3 2 1
+          1 2 3 4 5 4 3 2 1
+            1 2 3 4 3 2 1
+              1 2 3 2 1
+                1 2 1
+                  1
+*/
+import java.util.Scanner;
 public class Pattern4 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Decimal Number");
-		double n = sc.nextDouble();
-		int a = (int)n;
-		double b = n-a;
-		String res="";
-		if(a==0)
-			res=res+a;
-		while(a>0)
-		{
-			int t = a%2;
-			res = t+ res;
-			a=a/2;		
-		}
-		
-		if(b!=0)
-		{
-			int t=0;
-			res=res+".";
-			while(t<=3)
-			{
-				
-				b= b*2 -  (int)b*2;
-				res= res+ (int)b;
-				System.out.println(b);
-				t++;
-			}
-		}
-		System.out.println(res);
-	}
 
+
+	
+
+		Scanner sc = new Scanner(System.in);
+
+        //Taking noOfRows value from the user
+
+        System.out.println("How Many Rows You Want In Your Pyramid?");
+
+        int noOfRows = sc.nextInt();
+
+        //Initializing rowCount with noOfRows
+
+        int rowCount = noOfRows;
+
+        System.out.println("Here Is Your Pyramid");
+
+        //Implementing the logic
+
+        for (int i = 0; i < noOfRows; i++)
+        {
+            //Printing i*2 spaces at the beginning of each row
+
+            for (int j = 1; j <= i*2; j++)
+            {
+                System.out.print(" ");
+            }
+
+            //Printing j where j value will be from 1 to rowCount
+
+            for (int j = 1; j <= rowCount; j++)          
+            {
+                System.out.print(j+" ");
+            }
+
+            //Printing j where j value will be from rowCount-1 to 1
+
+            for (int j = rowCount-1; j >= 1; j--)
+            {               
+                System.out.print(j+" ");            
+            }
+
+            System.out.println();
+
+            //Decrementing the rowCount
+
+            rowCount--;
+        }
+    }
 }
